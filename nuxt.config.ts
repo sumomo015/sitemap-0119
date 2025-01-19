ce23ada4e8e6dab65aea222433c18677ebf4b452
sumomo015 <138793740+sumomo015@users.noreply.github.com>
2025-01-19 23:48:02 +0900

    Add @nuxt/content module to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/seo'],
+  modules: ['@nuxtjs/seo', '@nuxt/content'],
   site: {
     url: 'https://example.com',
     name: 'My Site',
